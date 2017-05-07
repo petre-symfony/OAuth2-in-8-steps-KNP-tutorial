ae34ccdbfab457d5cd70ac1e48aad1a6ff3560b7
petrero <petrero@gmail.com>
2017-05-07 08:45:12 +0300

    Getting the Token via Client Credentials
    Put the champagne away: we're not done yet. Typically, access tokens don't last forever. COOP tokens last for 24 hours, which means that tomorrow, our script will break.
    
    Letting the website do the client-credentials work for us was nice for testing, but we need to do it ourselves inside the script. Every OAuth server has an API endpoint used to request access tokens. If we look at the COOP API Authentication docs, we can see the URL and the POST parameters it needs:
    http://coop.apps.knpuniversity.com/token
    
    Parameters:
        client_id
        client_secret
        grant_type
    Let's update our script to first make this API request. Fill in the client_id, client_secret and grant_type POST parameters.
    
    With any luck, when you run it, you should see a JSON response with an access token and a few other details:
    
    {
      'access_token': 'fa3b4e29d8df9900816547b8e53f87034893d84c',
      'expires_in': 86400,
      'token_type': 'Bearer',
      'scope': 'chickens-feed'
    }

--- cron/collect_eggs.php
+++ cron/collect_eggs.php
@@ -10,7 +10,16 @@ $http = new Client('http://coop.apps.knpuniversity.com', array(
     )
 ));
 
-$request = $http->post('/api/166/eggs-collect');
+$request = $http->post('/token', null, array(
+  'client_id'     => 'Peter\'s Lazy CRON job',
+  'client_secret' => '4a368f96fb7bf29f35a862eb6a500a28',
+  'grant_type'    => 'client_credentials'   
+));
+$response = $request->send();
+$responseBody = $response->getBody(true);
+var_dump($responseBody); die;
+
+$request = $http->post('/api/1664/eggs-collect');
 $request->addHeader('Authorization', 'Bearer 44901de9f0598a4967e1c941e760a9ea75524f6d');
 $response = $request->send();
 
