8cfd7fce860a8526802ea643efbd5cd782ea6f62
petrero <petrero@gmail.com>
2017-05-07 08:28:26 +0300

    Client Credentials Grant Type
    The first OAuth grant type is called Client Credentials, which is the simplest of all the types. It involves only two parties, the client and the server. For us, this is our command-line script and the COOP API.
    
    Using this grant type, there is no 'user', and the access token we get will only let us access resources under the control of the application. When we make API requests using this access token, it's almost like we're logging in as the application itself, not any individual user. I'll explain more in a second.
    
    If you visit the application you created earlier, you'll see a nice 'Generate a Token' link that when clicked will fetch one. Behind the scenes, this uses client credentials, which we'll see more closely in a second.
    
    http://coop.apps.knpuniversity.com/token
        ?client_id=Your+Client+Name
        &client_secret=abcdefg
        &grant_type=client_credentials
    But for now, we can celebrate by using this token immediately to take actions on behalf of the application!
    
    Access Tokens in the API
    
    Exactly how to do this depends on the API you're making requests to. One common method, and the one COOP uses, is to send it via an Authorization Bearer header.
    
    GET /api/barn-unlock HTTP/1.1
    Host: coop.apps.knpuniversity.com
    Authorization: Bearer ACCESSTOKENHERE
    
    Update the script to send this header.
    
    When we run the script again, start celebrating, because it works! And now we have enough eggs to make an omlette :)
    
    {
      'action': 'eggs-collect',
      'success': true,
      'message': 'Hey look at that, 3 eggs have been collected! ',
      'data': 3
    }

--- cron/collect_eggs.php
+++ cron/collect_eggs.php
@@ -11,6 +11,7 @@ $http = new Client('http://coop.apps.knpuniversity.com', array(
 ));
 
 $request = $http->post('/api/1664/eggs-collect');
+$request->addHeader('Authorization', 'Bearer 44901de9f0598a4967e1c941e760a9ea75524f6d');
 $response = $request->send();
 
 echo $response->getBody();
