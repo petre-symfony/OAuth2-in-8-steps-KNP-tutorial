6c01d68d361152302288768322608f578c1b6ae5
petrero <petrero@gmail.com>
2017-05-07 11:32:02 +0300

    Copy code from branch 4

--- client/src/OAuth2Demo/Client/Controllers/CoopOAuthController.php
+++ client/src/OAuth2Demo/Client/Controllers/CoopOAuthController.php
@@ -21,7 +21,20 @@ class CoopOAuthController extends BaseController{
    * @return RedirectResponse
    */
   public function redirectToAuthorization(Request $request){
-    die('Implement this in CoopOAuthController::redirectToAuthorization');
+    $redirectUrl = $this->generateUrl(
+      'coop_authorize_redirect', 
+      array(), 
+      true
+    );
+    
+    $url = 'http://coop.apps.knpuniversity.com/authorize?'.http_build_query(array(
+      'response_type' => 'code',
+      'client_id'     => 'Peter Top Cluck',
+      'redirect_uri'  => $redirectUrl,
+      'scope'         => 'eggs-count profile'
+    ));
+    
+    return $this->redirect($url);
   }
 
   /**
@@ -38,6 +51,62 @@ class CoopOAuthController extends BaseController{
     // equivalent to $_GET['code']
     $code = $request->get('code');
 
-    die('Implement this in CoopOAuthController::receiveAuthorizationCode');
+    if(!$code){
+      $error = $request->get('error');
+      $errorDescription = $request->get('error_description');
+      
+      return $this->render('failed_authorization.twig', array(
+        'response' => array(
+          'error'             => $error,
+          'errorDescription'  => $errorDescription
+        )
+      ));
+    }
+    
+    // create our http client (Guzzle)
+    $http = new Client('http://coop.apps.knpuniversity.com', array(
+        'request.options' => array(
+            'exceptions' => false,
+        )
+    ));
+    $redirectUrl = $this->generateUrl(
+      'coop_authorize_redirect', 
+      array(), 
+      true
+    );
+    
+    $request = $http->post('/token', null, array(
+      'client_id'     => 'Peter Top Cluck',
+      'client_secret' => 'feb19d51c9211f0b93e11ee445f10c76',
+      'grant_type'    => 'authorization_code',
+      'code'          => $code,
+      'redirect_uri'  => $redirectUrl  
+    ));
+    $response = $request->send();
+    $responseBody = $response->getBody(true);
+    $responseArr = json_decode($responseBody, true);
+    if (!isset($responseArr['access_token'])){
+      return $this->render('failed_token_request.twig', array(
+        'response' => $responseArr ? $responseArr : $response
+      ));
+    }
+    
+    $accesToken = $responseArr['access_token'];
+    $expiresIn = $responseArr['expires_in'];
+    $expiresAt = new \DateTime('+' . $expiresIn . ' seconds');
+    
+    $request = $http->get('/api/me');
+    $request->addHeader('Authorization', 'Bearer '.$accesToken);
+    $response = $request->send();
+    $json = json_decode($response->getBody(), true);
+    
+    $user = $this->getLoggedInUser();
+    $user->coopAccessToken = $accesToken;
+    $user->coopUserId = $json['id'];
+    $user->coopAccessExpiresAt = $expiresAt;
+    $this->saveUser($user);
+    
+    //redirects to the homepage
+    return $this->redirect($this->generateUrl('home'));
   }
 }
