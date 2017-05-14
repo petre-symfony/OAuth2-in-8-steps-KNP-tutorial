14777661c9972e7e30315ae3a7eb6fa5c3cea6f7
petrero <petrero@gmail.com>
2017-05-14 10:31:39 +0300

    Copy code from branch 10

--- client/data/refresh_tokens.php
+++ client/data/refresh_tokens.php
@@ -14,4 +14,35 @@ $http = new Client('http://coop.apps.knpuniversity.com', array(
 /** @var \OAuth2Demo\Client\Storage\Connection $conn */
 $conn = $app['connection'];
 
-echo 'TODO';
+$expiringTokens = $conn->getExpiringTokens(new DateTime('+1 month'));
+
+foreach($expiringTokens as $userInfo){
+  $request = $http->post('/token', null, array(
+    'client_id'     => 'Peter Top Cluck',
+    'client_secret' => 'feb19d51c9211f0b93e11ee445f10c76',
+    'grant_type'    => 'refresh_token',
+    'refresh_token' => $userInfo['coopRefreshToken']
+  ));
+  $response = $request->send();
+  $responseBody = $response->getBody(true);
+  var_dump($responseBody);
+  $responseArr = json_decode($responseBody, true);
+  
+  // add some error handling here, in case the refresh token has expired
+  // in that case, we'll get an error response, instead of an access token
+  $accesToken = $responseArr['access_token'];
+  $expiresIn = $responseArr['expires_in'];
+  $expiresAt = new \DateTime('+'.$expiresIn.' seconds');
+  $refreshToken = $responseArr['refresh_token'];
+  $conn->saveNewTokens(
+    $userInfo['email'],
+    $accesToken,
+    $expiresAt,
+    $refreshToken
+  );
+  echo sprintf(
+    "Refreshing token for user %s: now expires %s\n\n",
+    $userInfo['email'],
+    $expiresAt->format('Y-m-d H:i:s')
+  );
+}
